9fd4d0c9fdd340e475109dd68fb82f5934dc12b2
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2024-06-19 22:26:54 +0700

    update : default filter

---
+++ addons/om_hospital/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0',
+    'category': 'Productivity',
+    'sequence': -100,
+    'summary': 'Hostpital Management Software',
+    'description': """Hostpital Management Software""",
+    'website': 'https://www.odoo.com',
+    'depends': ['sale', 'mail'],
+    'installable': True,
+    'data': [
+       'views/patient.xml',
+       'security/ir.model.access.csv',
+       'views/sale.xml',
+       'data/data.xml',
+    ],
+    'license': 'LGPL-3',
+    'application': True,
+    'auto_install': False,
+    'installable': True
+}
